38c865be18913f509afed6affe188e9a5078cefe
jn <jn@uvdata.dk>
2015-09-28 18:59:41 +0200

    NuspecView added

---
+++ NuspecReleaseNotes/Presenters/NuspecPresenter.cs
@@ -0,0 +1,14 @@
+﻿using NuspecReleaseNotes.Views.Interfaces;
+
+namespace NuspecReleaseNotes.Presenters
+{
+    public class NuspecPresenter
+    {
+        private readonly INuspecView _view;
+
+        public NuspecPresenter(INuspecView view)
+        {
+            _view = view;
+        }
+    }
+}
---
+++ NuspecReleaseNotes/Views/NuspecView.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Forms;
+using NuspecReleaseNotes.Views.Interfaces;
+
+namespace NuspecReleaseNotes.Views
+{
+    public partial class NuspecView : UserControl, INuspecView
+    {
+        public NuspecView()
+        {
+            InitializeComponent();
+        }
+    }
+}
